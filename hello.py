f92f0059e54e0914a0031e33b93ed415e9c578ec
woohaw <woohaw@126.com>
2017-11-02 11:39:58 +0800

    hello.py is the first file in git repository

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+print 'hello world!'
+age=input('Please input your age:')
+if age<18:
+    print 'you are a kid !'
+elif age<100:
+    print 'you are an adult !'
+else:
+    print 'you are almost die !'
